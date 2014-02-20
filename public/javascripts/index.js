bc0f7fd78a92aad3475eaa103ae4b1dfd173f8cc
Harrison Wray <hwray@stanford.edu>
2014-02-19 23:17:18 -0800

    Added list directions for detours (and markers).

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,6 +8,10 @@ var timerInterval;
 
 var searchMarkers = new Array();
 
+var origRoute; 
+
+var yelpListings; 
+
 var categories = ["coffee", "food", "shopping", "cafes", "nightlife"]; 
 var categoryColors = ["#A35E5A", "#FFA100", "#82105b", "#44a16c", "#379788"];
 
@@ -110,33 +114,37 @@ function initialize() {
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
-function routeButtonClick(e) {
-  e.preventDefault(); 
-  var start = document.getElementById("routeStart").value; 
-  var end = document.getElementById("routeEnd").value; 
-  var vehicle = $('input[name="vehicleOptions"]:checked').val();
+  function routeButtonClick(e) {
+    e.preventDefault(); 
+    var start = document.getElementById("routeStart").value; 
+    var end = document.getElementById("routeEnd").value; 
+    var vehicle = $('input[name="vehicleOptions"]:checked').val();
 
-  var vehicleString;
+    var vehicleString;
 
-  if (vehicle === 'DRIVING')
-    vehicleString = google.maps.TravelMode.DRIVING;
-  else if (vehicle === 'TRANSIT')
-    vehicleString = google.maps.TravelMode.TRANSIT;
-  else if (vehicle === 'BICYCLING')
-    vehicleString = google.maps.TravelMode.BICYCLING;
-  else if (vehicle === 'WALKING')
-    vehicleString = google.maps.TravelMode.WALKING;
+    if (vehicle === 'DRIVING')
+      vehicleString = google.maps.TravelMode.DRIVING;
+    else if (vehicle === 'TRANSIT')
+      vehicleString = google.maps.TravelMode.TRANSIT;
+    else if (vehicle === 'BICYCLING')
+      vehicleString = google.maps.TravelMode.BICYCLING;
+    else if (vehicle === 'WALKING')
+      vehicleString = google.maps.TravelMode.WALKING;
+
+    requestDirections(start, end, vehicleString, showDirections); 
+
+    setAlarm(5000);
+  }
 
 
-  var request = {
+  function requestDirections(start, end, vehicle, callback) {
+    var request = {
       origin: start,
       destination: end,
-      travelMode: vehicleString, 
+      travelMode: vehicle 
     };
 
-    directionsService.route(request, directionsCallback);
-
-    //setAlarm(5000);
+    directionsService.route(request, callback);
   }
 
 
@@ -197,10 +205,26 @@ function routeButtonClick(e) {
 
   function yelpCallback(data, textStatus, jqXHR) {
     
+    yelpListings = data; 
+
     var detoursDiv = document.getElementById("detourDisplay"); 
     for (var i in data) {
       if(data[i].length > 0) {
+        // add listing div to carousel
         detoursDiv.appendChild(createListing(data[i][0]));
+
+        // add marker to map
+        var addressString = data[i][0].location.display_address[0] + ", " + data[i][0].location.display_address[1]; 
+        var request = {
+          address: addressString
+        }
+        geocoder.geocode(request, function(result, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            addMarker(result[0].geometry.location.d, result[0].geometry.location.e); 
+          } else {
+            // error while geocoding address to lat-lng
+          }
+        }); 
       }
     }
   }
@@ -240,9 +264,10 @@ function geocodeCallback(results, status) {
 }
 
 
-function directionsCallback(response, status) {
+function showDirections(response, status) {
   if (status == google.maps.DirectionsStatus.OK) {
-    //console.log(response); 
+    // console.log(response); 
+    origRoute = response; 
     var steps = response.routes[0].legs[0].steps; 
     var distSinceLast = 0; 
     for (var i = 0; i < steps.length; i++) {
@@ -260,14 +285,14 @@ function directionsCallback(response, status) {
         for (var j = increment; j < points.length; j += increment) {
           var latlng = new google.maps.LatLng(step.lat_lngs[j].d, step.lat_lngs[j].e); 
           searchMarkers.push(latlng); 
-          addMarker(step.lat_lngs[j].d, step.lat_lngs[j].e); 
+          //addMarker(step.lat_lngs[j].d, step.lat_lngs[j].e); 
         }
       } else {
         var lat = step.end_location.d;
         var lng = step.end_location.e; 
         var latlng = new google.maps.LatLng(lat, lng); 
         searchMarkers.push(latlng); 
-        addMarker(lat, lng); 
+        //addMarker(lat, lng); 
       }
     }
     directionsDisplay.setDirections(response);
@@ -279,9 +304,40 @@ function directionsCallback(response, status) {
 
     //console.log(duration); 
 
-    //var timer = document.getElementById("tripTime"); 
-    //timer.innerHTML = "Your trip will take " + duration; 
 
+    var timer = document.getElementById("tripTime"); 
+    // timer.innerHTML = "Your trip will take " + duration; 
+  } else {
+
+  }
+}
+
+
+function listDirections(response, status) {
+  if (status == google.maps.DirectionsStatus.OK) {
+    var dirDiv = document.getElementById("listDirections"); 
+    var detourSteps = response.routes[0].legs[0].steps; 
+    var origSteps = origRoute.routes[0].legs[0].steps; 
+    for (var i = 0; i < origSteps.length; i++) {
+      if (i >= detourSteps.length) 
+        break; 
+      
+      if (origSteps[i].localeCompare(detourSteps[i]) == 0) {
+        // step is the same as original route
+        console.log(origSteps[i]); 
+      } else {
+        // step diverges from the original route; follow detour steps from here on out
+        console.log("DIVERGING"); 
+        console.log("INSTEAD OF:"); 
+        console.log(origSteps[i]); 
+        console.log("DO THIS:"); 
+        console.log(detourSteps[i]); 
+        break; 
+      }
+    }
+    for (var j = i + 1; j < detourSteps.length; j++) {
+      console.log(detourSteps[j]); 
+    }
   } else {
 
   }
@@ -292,7 +348,8 @@ function addMarker(lat, lng) {
   var latlng = new google.maps.LatLng(lat, lng); 
   var marker = new google.maps.Marker({
     map: map,
-    position: latlng
+    position: latlng,
+    animation: google.maps.Animation.DROP
   });
 }
 
@@ -322,6 +379,8 @@ function secs2timeString(seconds){
   str += seconds;
   return str;
 }
+
+
 
 // var timeButton = document.getElementById("timeButton"); 
 // timeButton.addEventListener("click", function(e) {
